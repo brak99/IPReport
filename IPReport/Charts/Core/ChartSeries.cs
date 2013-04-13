394a02458427027514979aff40285f76808544ab
manns <manns>
2013-04-13 07:13:16 +0000

    cleanup

--- IPReport/Charts/Core/ChartSeries.cs
+++ IPReport/Charts/Core/ChartSeries.cs
@@ -28,41 +28,13 @@ namespace De.TorstenMandelkow.MetroChart
 
     public class ChartSeries : ItemsControl
     { 
-		//public static readonly DependencyProperty DisplayMemberProperty =
-		//    DependencyProperty.Register("DisplayMember",
-		//    typeof(string),
-		//    typeof(ChartSeries),
-		//    new PropertyMetadata(null));
-		//public static readonly DependencyProperty ValueMemberProperty =
-		//    DependencyProperty.Register("ValueMember",
-		//    typeof(string),
-		//    typeof(ChartSeries),
-		//    new PropertyMetadata(null));
+		
 		public static readonly DependencyProperty CaptionProperty =
 			DependencyProperty.Register("Caption",
 			typeof(string),
 			typeof(ChartSeries),
 			new PropertyMetadata("Caption"));
-		//public static readonly DependencyProperty PerformanceTargetMemberProperty =
-		//    DependencyProperty.Register("PerformanceTargetMember",
-		//    typeof(string),
-		//    typeof(ChartSeries),
-		//    new PropertyMetadata(null));
-		//public static readonly DependencyProperty ActualPerformanceMemberProperty =
-		//    DependencyProperty.Register("ActualPerformanceMember",
-		//    typeof(string),
-		//    typeof(ChartSeries),
-		//    new PropertyMetadata(null));
-		//public static readonly DependencyProperty ActualPerformanceProperty =
-		//    DependencyProperty.Register("ActualPerformance",
-		//    typeof(double),
-		//    typeof(ChartSeries),
-		//    new PropertyMetadata(null));
-		//public static readonly DependencyProperty PerformanceTargetProperty =
-		//    DependencyProperty.Register("PerformanceTarget",
-		//    typeof(double),
-		//    typeof(ChartSeries),
-		//    new PropertyMetadata(null));
+		
 
         public ChartSeries()
         {   
@@ -80,52 +52,6 @@ namespace De.TorstenMandelkow.MetroChart
             }
         }
 
-		//public string DisplayMember
-		//{
-		//    get
-		//    {
-		//        return (string)GetValue(DisplayMemberProperty);
-		//    }
-		//    set
-		//    {
-		//        SetValue(DisplayMemberProperty, value);
-		//    }
-		//}
-
-		//public string ValueMember
-		//{
-		//    get
-		//    {
-		//        return (string)GetValue(ValueMemberProperty);
-		//    }
-		//    set
-		//    {
-		//        SetValue(ValueMemberProperty, value);
-		//    }
-		//}
-
-		//public string PerformanceTargetMember
-		//{
-		//    get { return (string)GetValue(PerformanceTargetMemberProperty); }
-		//    set { SetValue(PerformanceTargetMemberProperty, value); }
-		//}
-
-		//public string ActualPerformanceMember
-		//{
-		//    get { return (string)GetValue(ActualPerformanceMemberProperty); }
-		//    set { SetValue(ActualPerformanceMemberProperty, value); }
-		//}
-
-		//public double ActualPerformance
-		//{
-		//    get { return (double)GetValue(ActualPerformanceProperty); }
-		//    set { SetValue(ActualPerformanceProperty, value); }
-		//}
-
-		//public double PerformanceTarget
-		//{
-		//    get { return (double)GetValue(PerformanceTargetProperty); }
-		//    set { SetValue(PerformanceTargetProperty, value); }
-		//}
+		
     }
 }
