bd583c85c4e4ebcec445565bf594ee7c8d585820
manns <manns>
2013-04-25 02:39:52 +0000

    initial revision

---
+++ IPReport/View/DecimalToPercentConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace IPReport.View
+{
+	public class DecimalToPercentConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			decimal decimalPercentage = (decimal)value;
+			return decimalPercentage * 100.0m;
+		}
+
+		public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
+		{
+			decimal percentage = System.Convert.ToDecimal(value);
+			return percentage / 100.0m;
+		}
+	}
+}
---
+++ IPReport/View/IntToMonthConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace IPReport.View
+{
+	public class IntToMonthConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			int monthNumber = (int)value;
+			return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(monthNumber);
+		}
+
+		public object ConvertBack(object value, Type targetTypes, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+	}
+}
