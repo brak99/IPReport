469dde721ff6076ab7da06c0c0b0f6892ef30e6f
manns <manns>
2014-01-03 03:56:12 +0000

    code coverage report

---
+++ IPReport.Test/RunCodeCoverage.bat
@@ -0,0 +1,3 @@
+..\packages\OpenCover.4.5.2316\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\NUnit 2.6.2\bin\nunit-console-x86.exe" -targetargs:"bin\Debug\IPReport.Test.dll /noshadow" targetdir:bin\Debug -output:opencovertests.xml
+
+..\packages\ReportGenerator.1.9.1.0\ReportGenerator.exe "-reports:opencovertests.xml" "-targetdir:coverageReport"
